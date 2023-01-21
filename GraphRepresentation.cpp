b39ebba5cdfd93fac30170870ac9a13f774f111e
Arpit Singh Verma <99022671+asv018@users.noreply.github.com>
2023-01-21 22:12:38 +0530

    Update GraphRepresentation.cpp

--- GraphRepresentation.cpp
+++ GraphRepresentation.cpp
@@ -1,1 +1,16 @@
+// Graph Representation
+int n , m;
+cin >> n >> m;
+// n - number of nodes
+// m - number of edges
 
+vector<int> adj[n + 1];
+for(int i = 0 ; i < n ; i++)
+{
+    int u , v;
+    cin >> u >> v;
+    //there is an edge between node u and v
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    
+}
